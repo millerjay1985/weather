c25817b283f66f73ce0c6151b252f89b00811bfc
Jon Miller <millerjay1985@users.noreply.github.com>
2016-03-29 23:51:44 -0400

    Create json_data.json.js

---
+++ assests/json/json_data.json.js
@@ -0,0 +1,7 @@
+myFunction({"sun_shower":""<div class=\"icon sun-shower\">
+  <div class=\"clou\"></div>
+  <div class=\"sun\">
+    <div class=\"rays\"></div>
+  </div>
+  <div class=\"rain\"></div>
+</div>"});

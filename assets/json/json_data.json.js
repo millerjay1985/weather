59fbf9524be0a6165c235073d260e5bd2a84f75f
Jon Miller <millerjay1985@users.noreply.github.com>
2016-03-30 01:46:36 -0400

    Update json_data.json.js

--- assets/json/json_data.json.js
+++ assets/json/json_data.json.js
@@ -1,5 +1,8 @@
 myFunction({
 	"sun_shower": "<div class=\"icon sun-shower\"><div class=\"cloud\"></div><div class=\"sun\"><div class=\"rays\"></div></div><div class=\"rain\"></div></div>",
 	"sunny": "<div class=\"icon sunny\"><div class=\"sun\"><div class=\"rays\"></div></div></div>",
-	"rainy": "<div class=\"icon rainy\"><div class=\"cloud\"></div><div class=\"rain\"></div></div>"
+	"rainy": "<div class=\"icon rainy\"><div class=\"cloud\"></div><div class=\"rain\"></div></div>",
+	"cloudy": "<div class=\"icon cloudy\"><div class=\"cloud\"></div><div class=\"cloud\"></div></div>",
+	"flurries": "<div class=\"icon flurries\"><div class=\"cloud\"></div><div class=\"snow\"><div class=\"flake\"></div><div class=\"flake\"></div></div></div>",
+	"thunder": "<div class=\"icon thunder-storm\"><div class=\"cloud\"></div><div class=\"lightning\"><div class=\"bolt\"></div><div class=\"bolt\"></div></div></div>"
 });

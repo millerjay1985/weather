65bd4c827b0a3423fa99b6480bc6c8c34beb705b
Jon Miller <millerjay1985@users.noreply.github.com>
2016-03-29 23:54:07 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,6 +82,19 @@ $(function(){
         document.getElementById("symbol").innerHTML = symbols[x];
         x = 1 - x;
     }
+    
+    function ajax_test(callback){  
+   $.ajax({
+      //this is the url to my json.js file
+      url: "http://millerjay1985.github.io/gh-pages-test/json_data.json.js",
+      dataType: "jsonp",
+      //this is the name of the function in my .json.js file
+      jsonpCallback: "myFunction",
+      success: function(data) {  
+        callback(data);
+     }
+   });  
+}  
 
     /* Error handling functions */
 

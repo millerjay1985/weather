f8be84aaad5c8ea8845aac2e1c8eebf6366fd2bf
Jon Miller <millerjay1985@users.noreply.github.com>
2016-03-30 00:43:38 -0400

    Update json_data.json.js

--- assets/json/json_data.json.js
+++ assets/json/json_data.json.js
@@ -1,1 +1,3 @@
-myFunction({"sun_shower":"<div class=\"icon sun-shower\"><div class=\"clou\"></div><div class=\"sun\"><div class=\"rays\"></div></div><div class=\"rain\"></div></div>"});
+myFunction({"sun_shower":"<div class=\"icon sun-shower\"><div class=\"clou\"></div><div class=\"sun\"><div class=\"rays\"></div></div><div class=\"rain\"></div></div>",
+  "sunny":"<div class=\"icon sunny\"><div class=\"sun\"><div class=\"rays\"></div></div></div>"
+});
